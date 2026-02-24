b1ff28137f0b972df1b34c93e16cb3731ac8390a
Justin <jut.harris@gmail.com>
2026-02-24 07:51:25 -0500

    Update types.ts

--- types.ts
+++ types.ts
@@ -46,6 +46,6 @@ export interface SubmaxTest {
   elapsedStartSec: number;
   elapsedEndSec: number;
   summary: any;
-  data: any; // Stores the 'miles' or 'segments' array
+  data: any; 
   createdAt: string;
 }
