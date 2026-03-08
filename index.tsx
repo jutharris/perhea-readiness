4031abf31bc9c797d46356a1bf091e300ab05b32
Justin <jut.harris@gmail.com>
2026-03-07 20:53:38 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-console.log("App Initializing...");
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
